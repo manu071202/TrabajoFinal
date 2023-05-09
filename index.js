d43d6cc7bb3e90b9b31ca7ed19c44842cca672f1
manu <manuelibanezastorga@gmail.com>
2023-05-09 19:33:19 -0300

    primer commit para repartir tarreas de base de datos

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const app = express();
+dotenv.config();
+
+/*routes */
+require("./database/database")
+app.set("PORT", process.env.PORT || 5000);
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(app.get("PORT"), () => {
+    console.log(`servidor ejecutandoser en puerto
+ ${app.get("PORT")}`);
+});
---
+++ src/database/database.js
@@ -0,0 +1,19 @@
+//mongodb+srv://nahusepatagua:<password>@cluster0.deu6brs.mongodb.net/?retryWrites=true&w=majority
+
+
+const mongoose = require("mongoose")
+
+let mongoUri = "mongodb+srv:nahusepatagua:contraseña@cluster0.deu6brs.mongodb.net/?retryWrites=true&w=majority"
+//mongoose.set("useCreateIndex", false)
+
+mongoose
+  .connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB connected successfully!");
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDB: ", err);
+  });
